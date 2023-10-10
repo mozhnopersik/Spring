aa5905d9a191b3a5a8968d95f52ac0fdbdeec8c6
mozhnopersik <mozhnopersik@mail.ru>
2023-10-10 23:03:49 +0300

    All changes

--- Spring/ViewControllers/SpringAnimationViewController.swift
+++ Spring/ViewControllers/SpringAnimationViewController.swift
@@ -6,24 +6,65 @@
 //
 
 import UIKit
+import SpringAnimation
 
 class SpringAnimationViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    // MARK: - IB Outlets
+    
+    @IBOutlet weak var middleBall: SpringView!
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLayoutSubviews() {
+        middleBall.layer.cornerRadius = middleBall.frame.size.width / 2
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - IB Actions
+    
+    @IBAction func squeezeRight(_ sender: SpringButton) {
+        middleBall.animation = "squeezeRight"
+        middleBall.animate()
     }
-    */
+    
+    @IBAction func squeezeLeft(_ sender: SpringButton) {
+        middleBall.animation = "squeezeLeft"
+        middleBall.animate()
+    }
 
+    @IBAction func squeezeDown(_ sender: SpringButton) {
+        middleBall.animation = "squeezeDown"
+        middleBall.animate()
+    }
+    
+    @IBAction func flipX(_ sender: SpringButton) {
+        middleBall.animation = "flipX"
+        middleBall.animate()
+    }
+    
+    @IBAction func flipY(_ sender: SpringButton) {
+        middleBall.animation = "flipY"
+        middleBall.animate()
+    }
+
+    @IBAction func morph(_ sender: SpringButton) {
+        middleBall.animation = "morph"
+        middleBall.animate()
+    }
+    @IBAction func fadeIn(_ sender: SpringButton) {
+        middleBall.animation = "fadeIn"
+        middleBall.animate()
+    }
+
+    @IBAction func wobble(_ sender: SpringButton) {
+        middleBall.animation = "wobble"
+        middleBall.animate()
+    }
+
+    @IBAction func fadeOut(_ sender: SpringButton) {
+        middleBall.animation = "fadeOut"
+        middleBall.animate()
+    }
+    
 }
+
